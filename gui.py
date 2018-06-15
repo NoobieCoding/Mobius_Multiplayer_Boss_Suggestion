969c59abcc4b8f7ecf51f21150cebd436cc8c67d
Nuttapong Rojanavanich <tuter555spectrum@gmail.com>
2018-06-15 15:51:04 +0900

    Create ui's base outline

--- gui.py
+++ gui.py
@@ -1,9 +1,14 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 
-class UI(Widget):
-    pass
+class UI(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(UI, self).__init__(**kwargs)
 
 
 class MobiusMPSuggestionApp(App):
