a776e3b662bba8296182b9da34255ea21900e316
Nuttapong Rojanavanich <tuter555spectrum@gmail.com>
2018-06-15 15:59:39 +0900

    Update test for cli

--- test_main.py
+++ test_main.py
@@ -2,30 +2,28 @@ import data
 import sys
 import json
 import unittest
+import suggestion_system
 
 
 class Test(unittest.TestCase):
-    jobsData = {}
-
-    def setUp(self):
-        global jobsData
-        with open('jobs.json', 'r') as f:
-            jobsData = json.load(f)
 
     def testGetData1(self):
-        allSuggestedJobs = jobsData["suggestion"]["fire"]["attacker"]
-        assert len(allSuggestedJobs) == 4
-        assert allSuggestedJobs[0] == "天道士"
+        suggestedJobs = (suggestion_system
+                         .get_suggested_jobs_name('fire', 'attacker'))
+        assert len(suggestedJobs) == 4
+        assert suggestedJobs[1] == "天道士"
 
     def testGetData2(self):
-        allSuggestedJobs = jobsData["suggestion"]["light"]["breaker"]
-        assert len(allSuggestedJobs) == 5
-        assert allSuggestedJobs[3] == "マテリアハンター"
+        suggestedJobs = (suggestion_system
+                         .get_suggested_jobs_name('light', 'breaker'))
+        assert len(suggestedJobs) == 5
+        assert suggestedJobs[3] == "マテリアハンター"
 
     def testGetDataError(self):
         with self.assertRaises(Exception):
-            allSuggestedJobs = jobsData["suggestion"]["ice"]["breaker"]
-            
+            suggestedJobs = (suggestion_system
+                             .get_suggested_jobs_name('god', 'support'))
+
 
 if __name__ == '__main__':
     unittest.main()
